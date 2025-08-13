388693a161873be29169bbd488fc16a139edeae9
Cesar Chavez <cesarchamal@gmail.com>
2025-08-13 19:57:40 +0200

    Initial commit.

---
+++ src/main/java/com/cleancode/exercise/refactor_better/OrderProcessor.java
@@ -0,0 +1,23 @@
+package com.cleancode.exercise.refactor_better;
+
+public class OrderProcessor {
+    public void processOrder(String customer, int t, boolean v) {
+        if (t == 1) {
+            System.out.println("Processing regular order for " + customer);
+        } else if (t == 2) {
+            System.out.println("Processing rush order for " + customer);
+        } else {
+            System.out.println("Unknown order type for " + customer);
+        }
+
+        if (v) {
+            System.out.println("Sending confirmation email...");
+            // simulate email
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
